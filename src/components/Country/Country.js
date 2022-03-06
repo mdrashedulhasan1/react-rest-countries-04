ff9489687ef783c54b1728f87cf436b2b5ef283f
Rashedul Hasan <Rashedul Hasan>
2022-03-07 00:09:23 +0600

    first commit

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -4,9 +4,9 @@ const Country = (props) => {
     const { region, name, capital, population, flag } = props.country;
     console.log(props.country);
     return (
-        <div className='country-container'>
-            <img src={flag} alt="" />
-            <h3>Name: {name}</h3>
+        <div className='country-container bg-primary'>
+            <img className='rounded-3' src={flag} alt="" />
+            <h3 className='text-warning'>Name: {name}</h3>
             <h4>{capital}</h4>
             <h5>population: {population}</h5>
             <h5>region: {region}</h5>
